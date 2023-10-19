98cbe6ba076362bf3897464c4dbc98d3ed1baff1
computer world <faradayotuoniyo@gmail.com>
2023-10-19 21:54:58 +0100

    added gspread_code.py which was left out in last commit

--- project-1/gspread_code.py
+++ project-1/gspread_code.py
@@ -1,60 +1,79 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 16 15:12:29 2023
-
-@author: computer world
-
-Project : getting data from github repositories(through selenium library) and 
-sending to google spreadsheet with gspread package
-"""
-
+import os
+import logging 
 import gspread
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-path = "documents/datasets_for_data_science/gspread_project/spartan-figure-402211-2241d0349aab.json"
+logging.basicConfig(level=logging.DEBUG, 
+                    format="%(levelname)s (%(asctime)s) : %(message)s (%(lineno)d)")
+ 
+current_working_directory = os.getcwd()
+#project_json is the credential file for the service account created
+project_json = "spartan-figure-402211-2241d0349aab.json"
+path = os.path.join(current_working_directory,project_json)
+
+#url for github repo stored in variable github_url
+github_url = "https://github.com/Humphery7?tab=repositories"
 
 def update_spreadsheet(spreadsheet = "Gspread SpreadSheet",worksheet="Sheet1"):
     
+    ''' authenticates python script to passed-in spreadsheet, 
+        gets data from github repo and updates the chosen worksheet of the spreadsheet
+        with the data to ''' 
+        
+    
+    #loading credentials and authorizing gspread
     client = gspread.service_account(filename=path)
     worksheet = client.open(spreadsheet).worksheet(worksheet)
-    worksheet.clear() #clearning worksheet on each run of program
+    #clearning worksheet on each run of program
+    worksheet.clear() 
     
-    def github_data(url = "https://github.com/Humphery7?tab=repositories"):
-        
-        options = webdriver.ChromeOptions() 
-        driver = webdriver.Chrome(options=options) #instantiate driver
-        driver.set_window_size(1120,1000)
-        driver.get(url) 
-        driver.implicitly_wait(30) #waiting for data to be available implicitly
-        
-        github_repo = {} #dictionary to hold features and values of github data
-        RepoName = [] #list variable holding repository names
-        Language = [] #list variable holding language used
-        Description = [] #list variable holding Description of each repo
-        Datetime_Posted = [] #list variable holding datetime when repo was las updated/posted 
-        
-        try:
-            #Start Scapping from github repo with selenium
-            RepoName += [name.text for name in driver.find_elements(by=By.CSS_SELECTOR,value='a[itemprop="name codeRepository"]')]
-            Language += [language.text for language in driver.find_elements(by=By.CSS_SELECTOR,value='span[itemprop="programmingLanguage"]')]
-            Description += [desc.text for desc in driver.find_elements(by=By.CSS_SELECTOR, value='p[itemprop="description"]')]
-            Datetime_Posted += [time.text for time in driver.find_elements(by=By.CSS_SELECTOR,value='relative-time[class="no-wrap"]')] 
-        except Exception as e:
-            print("one or more of elements not found", e)
-            
-        #populating dictionary github_repo which will be used to create pandas dataframe
-        github_repo["RepoName"] = RepoName
-        github_repo["Language"] = Language
-        github_repo["Description"]= Description
-        github_repo['Datetime_posted'] = Datetime_Posted
-        
-        return pd.DataFrame(github_repo).reset_index(drop=True)
-
-    data = github_data() #calling github_data function and assigning output to data
-
-    worksheet.update([data.columns.tolist()] + data.values.tolist()) #updating to spreadsheet
     
+        
+    options = webdriver.ChromeOptions() 
+    #instantiate driver
+    driver = webdriver.Chrome(options=options) 
+    driver.set_window_size(1120,1000)
+    driver.get(github_url) 
+    #waiting for data to be available implicitly
+    driver.implicitly_wait(30) 
+    
+    #dictionary to hold features and values of github data
+    github_repo = {}
+    #list variable holding repository names
+    RepoName = [] 
+    #list variable holding language used
+    Language = [] 
+    #list variable holding Description of each repo
+    Description = [] 
+    #list variable holding datetime when repo was las updated/posted
+    Datetime_Posted = []  
+
+    #Getting Repository names from github repo  with selenium
+    RepoName += [name.text for name in driver.find_elements(by=By.CSS_SELECTOR,value='a[itemprop="name codeRepository"]')]
+    #Getting Language used for each repo from github repo  with selenium
+    Language += [language.text for language in driver.find_elements(by=By.CSS_SELECTOR,value='span[itemprop="programmingLanguage"]')]
+    #Getting Description of each repo from github repo  with selenium
+    Description += [desc.text for desc in driver.find_elements(by=By.CSS_SELECTOR, value='p[itemprop="description"]')]
+    #Getting Datetime when repo was posted or last updated from github repo  with selenium
+    Datetime_Posted += [time.text for time in driver.find_elements(by=By.CSS_SELECTOR,value='relative-time[class="no-wrap"]')] 
+        
+    #populating dictionary github_repo which will be used to create pandas dataframe
+    github_repo["RepoName"] = RepoName
+    github_repo["Language"] = Language
+    github_repo["Description"]= Description
+    github_repo['Datetime_posted'] = Datetime_Posted
+    
+    #attempting to create pandas dataframe and assigning to data variable
+    try:
+        data =  pd.DataFrame(github_repo).reset_index(drop=True)
+    except Exception as e:
+        logging.critical("one or more elements were not found", e)
+        exit(0)
+        
+    #updating to spreadsheet
+    worksheet.update([data.columns.tolist()] + data.values.tolist()) 
+
 
 update_spreadsheet()
